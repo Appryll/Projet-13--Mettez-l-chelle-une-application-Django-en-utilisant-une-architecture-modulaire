a4a6b5f4e3851544883e0ff3f2868ecfedd34681
Natalia FABIANO <natalia.fabiano@hotmail.com>
2022-09-15 22:46:51 +0200

    docker

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+# syntax=docker/dockerfile:1
+
+# base image -> img linux light
+FROM python:3.8-slim-buster 
+
+# ~python -B
+ENV PYTHONDONTWRITEBYTECODE=1
+# log in console ~ python -u
+ENV PYTHONUNBUFFERED=1
+
+# set work directory
+WORKDIR /code
+
+# copy just the requirements.txt first to leverage Docker cache
+# install all dependencies for Python app
+# what-where
+COPY requirements.txt requirements.txt
+
+# update pip & install dependencies in requirements.txt
+RUN python -m pip install --upgrade pip \ 
+&& pip install -r requirements.txt
+
+# copy all content to work directory /app
+COPY . .
+
+# run the application
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,4 +1,5 @@
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -8,12 +9,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -112,6 +113,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # MEDIA_URL = '/media/'
 
