caaa3140a3dc260e33b09360e8361b3e22edc67b
Natalia FABIANO <natalia.fabiano@hotmail.com>
2022-09-20 11:24:02 +0200

    modif settings-> heroku deploy server error

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY', default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'herokuapp.com']
 
 
 # Application definition
