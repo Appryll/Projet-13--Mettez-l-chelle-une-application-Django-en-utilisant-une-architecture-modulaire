314634697bc8a8481b08593e8fd9b447af4e9b31
Natalia FABIANO <natalia.fabiano@hotmail.com>
2022-09-20 11:57:50 +0200

    modif settings-> heroku deploy server error

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -2,6 +2,7 @@ import os
 from decouple import config
 from django.core.management.utils import get_random_secret_key
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,6 +125,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 django_heroku.settings(locals())
 
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
